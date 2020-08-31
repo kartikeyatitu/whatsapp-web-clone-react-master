1096843f841108038d4492f4ac0e3216edd297a6
Kartikeya <kartikeya2998@gmail.com>
2020-08-31 09:54:07 +0530

    Updates

---
+++ precache-manifest.d8f7534fc5c7e03e24b38b3ab9e3c7a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e5090f1cf924460052228e284ab5c9",
+    "url": "/whatsapp-web-clone-react-master/index.html"
+  },
+  {
+    "revision": "51ef5c52b8c42f1cf523",
+    "url": "/whatsapp-web-clone-react-master/static/css/main.504e44a1.chunk.css"
+  },
+  {
+    "revision": "999c672201b0c45e8286",
+    "url": "/whatsapp-web-clone-react-master/static/js/2.4baaaf42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whatsapp-web-clone-react-master/static/js/2.4baaaf42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ef5c52b8c42f1cf523",
+    "url": "/whatsapp-web-clone-react-master/static/js/main.54ad0a35.chunk.js"
+  },
+  {
+    "revision": "7761a3c825dff92bb0ac",
+    "url": "/whatsapp-web-clone-react-master/static/js/runtime-main.a9d04b2a.js"
+  },
+  {
+    "revision": "0782f2c66c28702a882b2b0f26f293a2",
+    "url": "/whatsapp-web-clone-react-master/static/media/bb6.0782f2c6.png"
+  },
+  {
+    "revision": "211c1153d75a5941a214cc29314c222f",
+    "url": "/whatsapp-web-clone-react-master/static/media/done_all.211c1153.svg"
+  },
+  {
+    "revision": "282a36f5ce69016ad280b7b2ebcbd186",
+    "url": "/whatsapp-web-clone-react-master/static/media/intro-whatsapp.282a36f5.jpg"
+  },
+  {
+    "revision": "958bf042217447a5282c358d5c23ff03",
+    "url": "/whatsapp-web-clone-react-master/static/media/mic.958bf042.svg"
+  },
+  {
+    "revision": "231fed550308fbb034a7b92d0a12bb52",
+    "url": "/whatsapp-web-clone-react-master/static/media/search.231fed55.svg"
+  },
+  {
+    "revision": "282ecb9e6de3a9602f6cb600cc32239d",
+    "url": "/whatsapp-web-clone-react-master/static/media/send.282ecb9e.svg"
+  },
+  {
+    "revision": "c50cc4490e6bcb8a32f3b77676df03be",
+    "url": "/whatsapp-web-clone-react-master/static/media/tag_faces.c50cc449.svg"
+  }
+]);
